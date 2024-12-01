43c7db684a47a507d2d541659d9afddc81849536
Andreza Morais <andrezamorais2090@gmail.com>
2024-11-30 21:00:27 -0300

    tentando pegar valores das teclas

---
+++ app.js
@@ -0,0 +1,22 @@
+//mudar o valor que aparece no display
+
+let nossoDisplay = document.querySelector(".display_piscar");
+
+let teclasDaCalculadora = document.getElementsByClassName(".elementoCalculadora");
+
+while (teclasDaCalculadora != '+'){
+    nossoDisplay.innerHTML = teclasDaCalculadora;
+}
+
+
+
+// function numeral(numerais){
+    
+  
+//         nossoDisplay.innerHTML = numerais;
+//         nossoDisplay.style.animation = "none";
+    
+   
+// }
+
+
